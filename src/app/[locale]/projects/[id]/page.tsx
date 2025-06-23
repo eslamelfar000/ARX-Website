be962838264a210e0785fb4ee08bbd7509761033
eslamelfar000 <eslamsaber707@gmail.com>
2025-06-23 18:58:13 +0300

    again

--- src/app/[locale]/projects/[id]/page.tsx
+++ src/app/[locale]/projects/[id]/page.tsx
@@ -637,9 +637,7 @@ const ProjectPage: React.FC = () => {
             <div className="w-full h-[300px] md:h-[500px] lg:h-[600px] xl:h-full relative rounded-3xl">
               {/* Map image from import */}
               <iframe
-                src={
-                  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3456.7890123456789!2d31.12345678901234!3d30.12345678901234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzDCsDA3JzM0LjQiTiAzMcKwMDcnMzQuNCJF!5e0!3m2!1sen!2seg!4v1234567890123"
-                }
+                src={projectData?.location_link || ""}
                 width="100%"
                 height="100%"
                 className="rounded-3xl"
